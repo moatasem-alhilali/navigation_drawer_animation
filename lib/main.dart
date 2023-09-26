f593b9a77fcc75104ee2c02085d35396f5c94359
moatasem <programmerms17@gmail.com>
2023-09-26 06:34:50 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:example_app/navigation_drawer/pages/navigation_drawer_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: NavigationDrawerCustom(),
+    );
+  }
+}
